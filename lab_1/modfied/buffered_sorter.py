de7d683f88e70ba9d9ff1856e05673138f8bc63a
v-Wint <veselyjburunduk@gmail.com>
2022-10-02 16:12:13 +0300

    lab 1: code improvements, comments

--- lab_1/modfied/buffered_sorter.py
+++ lab_1/modfied/buffered_sorter.py
@@ -5,30 +5,38 @@ import math
 
 class BufferedSorter(Sorter):
     def __init__(self, path_a: str, path_b: str, path_c: str, n: int):
+        """n - number of 32-bit numbers in the initial buffer"""
         super().__init__(path_a, path_b, path_c)
-        self.iteration = 0
-        self.split_n = 1
-        self.n = n
+        self.iteration = 0  # current iteration
+        self.split_n = 1  # current number of chunks written in split function
+        self.n = n  # number of numbers in the buffer
+
         try:
-            self.iter_max = int(int(math.log2(os.stat(self.path_a).st_size // 32 // self.n))) + 1
+            self.iter_max = math.ceil(math.log2(os.stat(self.path_a).st_size / 32 / self.n))
         except ValueError:
+            self.iter_max = 1
+        except FileNotFoundError:
             self.iter_max = 1
 
     def create_random_file(self, n: int, max_n: int):
         super().create_random_file(n, max_n)
-        self.iter_max = int(int(math.log2(os.stat(self.path_a).st_size // 32 // self.n))) + 1
+        self.iter_max = math.ceil(math.log2(os.stat(self.path_a).st_size / 32 / self.n))
 
     def copy_from_file(self, path: str):
         super().copy_from_file(path)
-        self.iter_max = int(int(math.log2(os.stat(self.path_a).st_size // 32 // self.n))) + 1
+        self.iter_max = math.ceil(math.log2(os.stat(self.path_a).st_size / 32 / self.n))
 
     def buffered_sort(self):
-        """Around 2 * 32 * n of RAM used max while buffering"""
+        """Around 32 * n of RAM used max while buffering"""
         self._buffered_prepare()
         for _ in range(self.iter_max):
             self._buffered_distribute()
-            self._merge()
+            self._buffered_merge()
             self.iteration += 1
+            self.split_n *= 2
+
+        self.iteration = 0
+        self.split_n = 1
 
     def _buffered_prepare(self):
         """Read chunks from file A, write sorted chunks into temporary file, copy it to file A"""
@@ -64,34 +72,90 @@ class BufferedSorter(Sorter):
         os.remove("tmp")
 
     def _buffered_distribute(self):
+        """Distribute between files in chunks of n by 2**iteration in a row"""
         a = open(self.path_a, "rb")
         b = open(self.path_b, "wb")
         c = open(self.path_c, "wb")
 
-        buffer = []
-        for _ in range(self.n):
-            x = a.read(32)
-            if not x:
-                break
-            buffer += x,
-
         i = True
-        j = 2
-        while buffer:
-            for x in buffer:
-                b.write(x) if i else c.write(x)
-
-            if j > self.split_n:
-                i = not i
-                j = 1
-            j += 1
-
-            del buffer
-            buffer = []
+        j = 1
+        while True:
             for _ in range(self.n):
                 x = a.read(32)
                 if not x:
-                    break
-                buffer += x,
-        self.split_n *= 2
+                    a.close(), b.close(), c.close()
+                    return
+                b.write(x) if i else c.write(x)
+            j += 1
+            if j > self.split_n:
+                i = not i
+                j = 1
+
+    def _buffered_merge(self):
+        """We know that we've got series of n * 2**iteration after distribution, so we can skip identifying the ned of
+        the series"""
+        a = open(self.path_a, "wb")
+        b = open(self.path_b, "rb")
+        c = open(self.path_c, "rb")
+
+        lim = self.n * self.split_n
+
+        curr_b = b.read(32)
+        curr_c = c.read(32)
+        i_b = 0
+        i_c = 0
+
+        while i_b < lim or i_c < lim:
+            if curr_b <= curr_c:
+                a.write(curr_b)
+                i_b += 1
+                curr_b = b.read(32)
+            else:
+                a.write(curr_c)
+                curr_c = c.read(32)
+                i_c += 1
+            # If the end of the series, simply write the remaining part of the opposite file
+            if i_b == lim:
+                while i_c < lim:
+                    a.write(curr_c)
+                    curr_c = c.read(32)
+                    i_c += 1
+            elif i_c == lim:
+                while i_b < lim:
+                    a.write(curr_b)
+                    i_b += 1
+                    curr_b = b.read(32)
+
+        while curr_b and curr_c:
+            i_b = 0
+            i_c = 0
+
+            while i_b < lim or i_c < lim:
+                if curr_b <= curr_c:
+                    a.write(curr_b)
+                    i_b += 1
+                    curr_b = b.read(32)
+                else:
+                    a.write(curr_c)
+                    curr_c = c.read(32)
+                    i_c += 1
+                if i_b == lim:
+                    while i_c < lim:
+                        a.write(curr_c)
+                        curr_c = c.read(32)
+                        i_c += 1
+                elif i_c == lim:
+                    while i_b < lim:
+                        a.write(curr_b)
+                        i_b += 1
+                        curr_b = b.read(32)
+        # write remaining one serie
+        while curr_b:
+            a.write(curr_b)
+            curr_b = b.read(32)
+        # write remaining one serie
+        while curr_c:
+            a.write(curr_c)
+            curr_c = c.read(32)
+
         a.close(), b.close(), c.close()
--- lab_1/modfied/main.py
+++ lab_1/modfied/main.py
@@ -2,10 +2,9 @@ from buffered_sorter import BufferedSorter
 
 
 def main():
-    sorter = BufferedSorter("a.bin", "b.bin", "c.bin", 1024**2//8)
-    sorter.copy_from_file("32Gb.bin")
-    sorter.sort()
-    print(sorter)
+    sorter = BufferedSorter("a.bin", "b.bin", "c.bin", 1024**3//8)
+    sorter.copy_from_file("32GBFILE.bin")
+    sorter.buffered_sort()
 
 
 if __name__ == "__main__":
